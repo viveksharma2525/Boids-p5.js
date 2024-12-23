3eaf8e4a8a934d1506ace9063607624a80a04b1c
viveksharma2525 <sharma.vivek0910@outlook.com>
2024-12-23 12:28:42 +0530

    initial

---
+++ README.md
@@ -0,0 +1,57 @@
+# Boids Simulation  
+[Live Preview](https://ishanmanchanda.github.io/Boids/) (Best viewed on laptop/desktop)  
+
+This project is inspired by *The Coding Train's* Boids coding challenge on YouTube. It recreates Craig Reynolds' Boids model using p5.js, demonstrating emergent flocking behavior of bird-like agents called Boids.  
+
+---
+
+## About the Simulation  
+The simulation showcases how simple rules applied to individual agents can lead to complex group behavior. Boids interact based on three core principles:  
+
+1. **Separation**: Avoid crowding by maintaining personal space.  
+   - Boids steer away from nearby agents to prevent overlaps.  
+2. **Alignment**: Align movement with nearby Boids.  
+   - Boids adjust their direction to match the average velocity of neighbors.  
+3. **Cohesion**: Move toward the local group's center.  
+   - Boids are attracted to the average position of nearby agents.  
+
+### How It Looks in Action  
+- **A single Boid in isolation** moves freely in the environment.  
+- **A small group of Boids** begins to cluster and align, moving as a flock.  
+- **With many Boids**: Complex patterns emerge, mimicking bird flocks or fish schools.  
+
+Adding **Predators** introduces new dynamics:  
+- Boids attempt to flee from nearby Predators.  
+- Predators pursue Boids using a "look-ahead" feature that predicts their paths.
+
+---
+
+## Additional Features  
+- **Predators**:  
+  - Hunt Boids by predicting their positions and velocities.  
+  - Have a hunger system: Starvation leads to death.  
+- **Sight Ranges**: Boids and Predators have limited perception for realistic interactions.  
+- **Dynamic Interactions**: Group behavior and wrap-around edges ensure agents interact naturally.  
+
+---
+
+## Controls  
+- **Behavior Toggles**:  
+  - Press `A` for Alignment, `C` for Cohesion, and `S` for Separation.  
+- **Add Agents**:  
+  - Hold `B` (Boids) or `P` (Predators) and click within the simulation area.  
+
+*A laptop or desktop is recommended for optimal experience.*  
+
+### Customization  
+Simulation parameters like initial values, agent sizes, sight ranges, and interaction weights can be modified in `globals.js`.  
+
+---
+
+## Technical Highlights  
+- **OOP Principles**: Implemented with inheritance and polymorphism.  
+- **Optimizations**: Uses bin-lattice spatial subdivision for efficient performance.  
+- **Vector Math**: Extensive use and optimization of vector operations.  
+
+---
+
---
+++ src/sketch.js
@@ -0,0 +1,111 @@
+
+
+// Global variables - START ====
+let DEBUG = true;
+let ACCURATE = false; // If this is `false` then we are using `maxNearCount` to reduce calculation.
+
+const width = 1100;
+const height = 700;
+
+const flock = [];
+const flockPool = [];
+// Agent count
+let count = 1500;
+const maxCount = 5000;
+// Maximum number which is used for one agent to steer to. Statistically, it is correct and a good optimisation.
+// On a 2016 MacBook Pro, 1500 agents are still 20 FPS with debug and without accurate calculation.
+const maxNearCount = 100;
+
+// Also good for speed up things, if we reduce the radius, but let leave it at 100.
+const perceptionRadius = 100;
+const maxForce = 0.2;
+const maxSpeed = 4;
+
+let alignSlider, cohesionSlider, separationSlider, countSlider;
+let alignValue, cohesionValue, separationValue, countValue;
+let aSliderValue = 1, cSliderValue = 1, sSliderValue = 1;
+
+/**
+ * Basic space subdivision - it is helpful above ~500 agent at the start when they are evenly distributed.
+ * The 3rd parameter is important to use the right size buckets. So, 9 buckets will have every agent near.
+ */
+const subdiv = new SubDiv(width, height, perceptionRadius);
+// Global variables - END =====
+
+function setup() {
+	createCanvas(width, height);
+
+	createDiv('Alignment:').class('sliderLabel');
+	alignSlider = createSlider(0, 5, aSliderValue, 0.1).class('slider');
+	alignSlider.attribute('oninput', `dispaySliderValue(alignSlider, alignValue)`);
+	alignValue = createDiv(aSliderValue).class('sliderValue');
+
+	createDiv('Cohesion:').class('sliderLabel');
+	cohesionSlider = createSlider(0, 5, cSliderValue, 0.1).class('slider');
+	cohesionSlider.attribute('oninput', `dispaySliderValue(cohesionSlider, cohesionValue)`);
+	cohesionValue = createDiv(cSliderValue).class('sliderValue');
+
+	createDiv('Separation:').class('sliderLabel');
+	separationSlider = createSlider(0, 5, sSliderValue, 0.1).class('slider');
+	separationSlider.attribute('oninput', `dispaySliderValue(separationSlider, separationValue)`);
+	separationValue = createDiv(sSliderValue).class('sliderValue');
+
+	const dbgCheckbox = createCheckbox('Debug', DEBUG).class('checkbox');
+	dbgCheckbox.changed(() => DEBUG = dbgCheckbox.checked());
+
+	const accCheckbox = createCheckbox('Accurate', ACCURATE).class('checkbox');
+	accCheckbox.changed(() => ACCURATE = accCheckbox.checked());
+
+	document.body.appendChild(document.createElement('br'));
+
+	createDiv('Agent count:').class('sliderLabel agent');
+	countSlider = createSlider(100, maxCount, count, 50).class('slider');
+	countSlider.attribute('oninput', `dispaySliderValue(countSlider, countValue)`);
+	countValue = createDiv(count).class('sliderValue');
+
+	for (let i = 0; i < maxCount; i++) {
+		flockPool.push(new Agent());
+	}
+
+	updateCount(count);
+}
+
+function dispaySliderValue(slider, val) {
+	val.html(slider.value());
+
+	// update all values
+	aSliderValue = alignSlider.value();
+	cSliderValue = cohesionSlider.value();
+	sSliderValue = separationSlider.value();
+
+	updateCount(countSlider.value());
+}
+
+function updateCount(c) {
+	if (c === flock.length) return;
+
+	count = c;
+
+	if (flock.length < count) {
+		while (flock.length < count) {
+			flock.push(flockPool[flock.length]);
+		}
+	} else {
+		while (flock.length > count) {
+			flock.pop();
+		}
+	}
+}
+
+function draw() {
+	background(64);
+
+	flock.forEach(agent => {
+		agent.update();
+	});
+
+	if (DEBUG) {
+		subdiv.getDebugGrid();
+		subdiv.getNearItems(flock[0], true);
+	}
+}
